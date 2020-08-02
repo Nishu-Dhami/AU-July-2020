7d22ad0e8bf0708a4e486447e700c1a2d0d65b1d
Nishu <nishudhami1@gmail.com>
2020-08-02 15:32:24 +0530

    Angular JS Assignment Added

--- AngularJS-Both/src/app/app.component.ts
+++ AngularJS-Both/src/app/app.component.ts
@@ -9,85 +9,125 @@ import { Component,OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
 
   title = 'AngularJS-session';
-  newUser = {first:'',last:'',age:"",empId:'',city:''};
-  user1 = {first:'Nishu',last:'Dhami',age:"21",empId:'INT445',city:'Gurugram'};
-  user2 = {first:'Kanak',last:'Yadav',age:"22",empId:'INT552',city:'Delhi'};
+  sorted=false;
+  newUser = {firstname:'',lastname:'',age:"",empId:'',loc:''};
+  user1 = {firstname:'Nishu',lastname:'Dhami',age:"21",empId:'INT445',loc:'Gurugram'};
+  user2 = {firstname:'Kanak',lastname:'Yadav',age:"22",empId:'INT552',loc:'Delhi'};
   name = [this.user1,this.user2];
-  delkey = null;
 
   ngOnInit(){ 
 
   }
- 
-   
+    
+  clear=()=>{
+      this.newUser.age="";
+      this.newUser.empId="";
+      this.newUser.firstname="";
+      this.newUser.loc="";
+      this.newUser.lastname="";
+      return;
+  }
   addUser = () =>{
-      let temp = {first:'',last:'',age:"",empId:'',city:''};
-      temp.first = this.newUser.first;
-      temp.last = this.newUser.last;
+      let temp = {firstname:'',lastname:'',age:"",empId:'',loc:''};
+      temp.firstname = this.newUser.firstname;
+      temp.lastname = this.newUser.lastname;
       temp.age = this.newUser.age;
       temp.empId = this.newUser.empId;
-      temp.city = this.newUser.city;
-      
+      temp.loc = this.newUser.loc;
       this.name.push(temp);
-     // newUser = {first:'',last:'',age:"",empId:'',city:''};
+      alert("Record AddedSuccessfully! \n Click 'CLEAR' to reset form");
       return ;
   } 
   
-  sortUser1 = () =>{
-     this.name.sort((a,b) => a.first.localeCompare(b.first));
+  sortUser=()=>{
+    alert("Click on column record to sort data column-wise(both asc & desc)");
+  }
+  sortbyfname = () =>{
+    if(!this.sorted)
+     { this.name.sort((a,b) => a.firstname.localeCompare(b.firstname));
+      this.sorted=true;}
+      else{
+        this.name.sort((a,b) => b.firstname.localeCompare(a.firstname));
+        this.sorted=false;
+      }
      return ;
   }
-  sortUser2 = () =>{
-    this.name.sort((a,b) => a.last.localeCompare(b.last));
+  sortbylname = () =>{
+    if(!this.sorted){
+    this.name.sort((a,b) => a.lastname.localeCompare(b.lastname));
+    this.sorted=true;
+    }
+    else
+    {
+      this.name.sort((a,b) => b.lastname.localeCompare(a.lastname));
+      this.sorted=false;
+    }
     return ;
  }
- sortUser3 = () =>{
+ sortbyage = () =>{
+   if(!this.sorted){
   this.name.sort((a,b) => a.age.localeCompare(b.age));
+  this.sorted=true;}
+  else{
+    this.name.sort((a,b) => b.age.localeCompare(a.age));
+    this.sorted=false;
+  }
   return ;
 }
-sortUser4 = () =>{
+sortbyempid = () =>{
+  if(!this.sorted){
   this.name.sort((a,b) => a.empId.localeCompare(b.empId));
+  this.sorted=true;
+  }
+  else{
+    this.name.sort((a,b) => b.empId.localeCompare(a.empId));
+    this.sorted=false;
+  }
   return ;
 }
-sortUser5 = () =>{
-  this.name.sort((a,b) => a.city.localeCompare(b.city));
+sortbyloc = () =>{
+  if(!this.sorted){
+  this.name.sort((a,b) => a.loc.localeCompare(b.loc));
+  this.sorted=true;
+  }
+  else
+  {
+    this.name.sort((a,b) => b.loc.localeCompare(a.loc));
+    this.sorted=false;
+  }
   return ;
 }
 
 updateUser= () =>{
-  let temp = {first:'',last:'',age:"",empId:'',city:''};
-      temp.first = this.newUser.first;
-      temp.last = this.newUser.last;
+  alert("You can edit user details based on Employee ID only");
+  let temp = {firstname:'',lastname:'',age:"",empId:'',loc:''};
+      temp.firstname = this.newUser.firstname;
+      temp.lastname = this.newUser.lastname;
       temp.age = this.newUser.age;
       temp.empId = this.newUser.empId;
-      temp.city = this.newUser.city;
-  var num : Number = null;
-  for(var i=0;i<this.name.length;i++){
-    var obj = this.name[i];
-    if(obj.empId == this.newUser.empId){
-      this.name[i] = temp;
-      break;
-    }
-  }
-  
-  
+      temp.loc = this.newUser.loc;
+      var num : Number = null;
+      for(var i=0;i<this.name.length;i++){
+        var obj = this.name[i];
+        if(obj.empId == this.newUser.empId){
+          this.name[i] = temp;
+          break;
+        }
+      }
+      
+      alert("Record Updated Successfully! \n Click 'CLEAR' to reset form");
 }
 
 deleteUser=()=>{
-  
-  
+  alert("Enter Employee ID you wanna delete!");
   for(var i=0;i<this.name.length;i++){
     var obj = this.name[i];
-    if(obj.empId == this.delkey){
+    if(obj.empId == this.newUser.empId){
       this.name.splice(i,1);
       break;
     }
   }
+  alert("Record Deleted Successfully! \n Click 'CLEAR' to reset form");
 }
-
-    
-    
    username : 'nishu';
-
-
 }
