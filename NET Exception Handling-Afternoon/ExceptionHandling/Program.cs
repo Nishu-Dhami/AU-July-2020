6639110fe37b5c7976f9a74d447a2a11b25e601c
Nishu <nishudhami1@gmail.com>
2020-08-05 11:57:27 +0530

    .NET Exception Handling Assignment Added

---
+++ NET Exception Handling-Afternoon/ExceptionHandling/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine(File.ReadAllText(@"C:\Users\nishu\Desktop\file.txt"));
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("File not found! '{0}' exception occured",e.Message);
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                Console.WriteLine("Directory not found! '{0}' exception occured",e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("'{0}' exception occured!",e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Finished...");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ NET Exception Handling-Afternoon/LOG/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+namespace LOG
+{
+    class Program
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main(string[] args)
+        {
+            Boolean flag = true;
+            log.Info("Starting Execution!!!");
+            try
+            {
+                string content = File.ReadAllText(@"C:\Users\nishu\Desktop\file.txt");
+                Console.WriteLine(content);
+            }
+            catch (FileNotFoundException ex)
+            {
+                flag = false;
+                Console.WriteLine("File not found! '{0}' exception occured".,ex);
+                log.Error(ex.StackTrace);
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                flag = false;
+                Console.WriteLine("Directory not found! C:\Users\nishu\Desktop caught '{0}' exception",ex.Message);
+                log.Error(ex.StackTrace);
+            }
+            catch (Exception ex)
+            {
+                flag = false;
+                Console.WriteLine("'{0}' exception occured",ex.Message);
+            }
+            finally
+            {
+                if (!flag)
+                {
+                    log.Warn("Unsuccessful Operation");
+                }
+                else
+                {
+                    Console.WriteLine("Finished . . .");
+                    log.Info("Program Execution Ended\n\n");
+                }
+                
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
